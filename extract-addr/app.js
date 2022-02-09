0f0bc116fa4a51a2c451f5017638e62f277e2bf6
ɐʇɯon3ɹ <contact.atmoner@gmail.com>
2022-02-09 16:59:08 +0100

    Create app.js

---
+++ extract-addr/app.js
@@ -0,0 +1,24 @@
+/*
+ * @atmon3r - 2022
+ */
+
+import cosmos from 'cosmos-lib'
+import cosmosConfig from './cosmos.config.js'
+
+function getAddressFromPubKey (pubKey, chain) {
+  let buff = new Buffer.from(pubKey, 'base64')
+  const address = cosmos.address.getAddress(buff, chain.coinLookup.addressPrefix)
+  console.log('Address: ' + address)
+  console.log('PubKey: ' + pubKey)
+  console.log(chain.explorer + address)    
+}
+
+cosmosConfig.forEach(function(getChain){
+  console.log(getChain.name)
+  getAddressFromPubKey(
+    'AzblIzESX2733gtn5h696tAeqmFPPafJ5CIKJWNrKpmg',
+    getChain
+  )
+  console.log('\n')
+});
+
